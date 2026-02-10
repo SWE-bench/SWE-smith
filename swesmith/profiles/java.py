41cddbd5e8440bd61e5d041cf59dfdc5157a4c9d
Ubuntu <ubuntu@ip-172-31-20-53.ec2.internal>
2026-02-10 07:09:45 +0000

    Minor change

--- swesmith/profiles/java.py
+++ swesmith/profiles/java.py
@@ -879,7 +879,7 @@ RUN apt-get update && apt-get install -y \
     && rm -rf /var/lib/apt/lists/*
 
 # Install Bazelisk
-RUN curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-arm64 -o /usr/local/bin/bazel && \
+RUN curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -o /usr/local/bin/bazel && \
     chmod +x /usr/local/bin/bazel
 
 
